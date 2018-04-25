e2853580a0932185cb70e6b1005a68c3cde4fd46
KrzysztofPakaszewski <krzysztof.pakaszewski@gmail.com>
2018-04-25 11:20:19 +0100

    Pesel

--- lab8/pesel/Pesel.cpp
+++ lab8/pesel/Pesel.cpp
@@ -5,6 +5,7 @@
 
 
 #include <vector>
+#include <sstream>
 #include "Pesel.h"
 using namespace academia;
 Pesel::Pesel(const char* a)
@@ -14,16 +15,36 @@ Pesel::Pesel(const char* a)
 }
 void Pesel::ValidatePesel(string a){
     if(a.size()!=11)
-        throw InvalidPeselLength("");
+        throw InvalidPeselLength(a,a.size());
+    for(int b=0;b<a.size();b++)
+    {
+        if(int(a[b])<48 ||int(a[b])>57)
+            throw InvalidPeselCharacter(a);
+    }
+    if(CheckSum(a) != int(a[10])-48)
+        throw InvalidPeselChecksum(a,CheckSum(a));
 
+}
+int Pesel::CheckSum(string a){
+    vector<int> temp;
+    for(int b=0;b<a.size();b++)
+    {
+        temp.emplace_back(int(a[b])%48);
+    }
+    int sum= temp[0]*9+7*temp[1]+3*temp[2]+temp[3]+temp[4]*9+7*temp[5]+3*temp[6]+temp[7]+9*temp[8]+7*temp[9];
+    return sum%10;
 }
 
 Pesel::Pesel() {}
 
 InvalidPeselLength::InvalidPeselLength(const string &__arg) : invalid_argument(__arg) {}
 
-InvalidPeselChecksum::InvalidPeselChecksum(const string &__arg) : invalid_argument(__arg) {}
+InvalidPeselCharacter::InvalidPeselCharacter(const string &__arg) : invalid_argument(__arg),arg(__arg) {}
 
-InvalidPeselCharacter::InvalidPeselCharacter(const string &__arg) : invalid_argument(__arg) {}
+AcademiaDataValidationError::AcademiaDataValidationError(const string &__arg) : runtime_error(__arg) {}
 
-AcademiaDataValidationError::AcademiaDataValidationError(const string &__arg) : invalid_argument(__arg) {}
+InvalidPeselChecksum::InvalidPeselChecksum(const string &__arg) : AcademiaDataValidationError(__arg) {}
+
+InvalidPeselChecksum::InvalidPeselChecksum(const string &__arg,int sum) :sum(sum),AcademiaDataValidationError(__arg),arg(__arg){}
+
+InvalidPeselLength::InvalidPeselLength(const string &__arg,int a):length(a),invalid_argument(__arg),arg(__arg){}
--- lab8/pesel/Pesel.h
+++ lab8/pesel/Pesel.h
@@ -10,23 +10,50 @@
 
 using namespace std;
 namespace academia {
-    class InvalidPeselChecksum :public invalid_argument {
+    class AcademiaDataValidationError :public runtime_error{
+    public:
+        AcademiaDataValidationError(const string &__arg);
+    };
+    class InvalidPeselChecksum :public AcademiaDataValidationError {
     public:
         InvalidPeselChecksum(const string &__arg);
+        InvalidPeselChecksum(const string &__arg,int sum);
+        const char* what() const throw(){
+            stringstream os;
+            os<<sum;
+            string temp = "Invalid PESEL("+ arg+") checksum: "+ os.str();
+            return temp.c_str();
+        }
 
+    protected:
+        string arg;
+        int sum;
     };
     class InvalidPeselCharacter : public invalid_argument{
     public:
         InvalidPeselCharacter(const string &__arg);
+        const char* what() const throw(){
+            string temp = "Invalid PESEL("+arg+") character set";
+            return temp.c_str();
+        }
+
+    protected:
+        string arg;
     };
 
     class InvalidPeselLength :public invalid_argument{
     public:
         InvalidPeselLength(const string &__arg);
-    };
-    class AcademiaDataValidationError :public invalid_argument{
-    public:
-        AcademiaDataValidationError(const string &__arg);
+        InvalidPeselLength(const string &__arg,int a);
+        const char* what() const throw(){
+            stringstream os;
+            os<<length;
+            string temp = "Invalid PESEL("+arg+") length: "+os.str();
+            return temp.c_str();
+        }
+    protected:
+        string arg;
+        int length;
     };
 
     class Pesel {
@@ -38,6 +65,7 @@ namespace academia {
 
         void ValidatePesel(string);
 
+        int CheckSum(string a);
 
     private:
         string pesel;
